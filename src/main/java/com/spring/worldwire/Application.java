fe7821a342785f2a63b0ce6bb88eb8acb156dd96
luxun1 <luxun@jd.com>
2018-04-18 10:45:42 +0800

    '修改一下'

--- src/main/java/com/spring/worldwire/Application.java
+++ src/main/java/com/spring/worldwire/Application.java
@@ -3,9 +3,10 @@ package com.spring.worldwire;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages="com.spring.worldwire")
-//@MapperScan("com.spring.worldwire.dao")
+@MapperScan(basePackages="com.spring.worldwire.dao")
 public class Application {
 	public static void main(String[] args) {
 		 SpringApplication.run(Application.class, args);
