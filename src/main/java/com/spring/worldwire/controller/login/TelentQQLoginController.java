d5faccee551255e44f10192bcbcad5ce2b03094b
luxun1 <luxun@jd.com>
2018-05-08 10:45:19 +0800

    'merge'

---
+++ src/main/java/com/spring/worldwire/controller/login/TelentQQLoginController.java
@@ -0,0 +1,7 @@
+package com.spring.worldwire.controller.login;
+
+/**
+ * Created by luxun on 2018/5/7.
+ */
+public class TelentQQLoginController {
+}
---
+++ src/main/java/com/spring/worldwire/utils/HttpUtils.java
@@ -0,0 +1,7 @@
+package com.spring.worldwire.utils;
+
+/**
+ * Created by luxun on 2018/5/7.
+ */
+public class HttpUtils {
+}
