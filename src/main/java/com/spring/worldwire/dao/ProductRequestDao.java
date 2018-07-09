29ad329618a9fc991d451f26416fa64e7128e4a5
luxun1 <luxun@jd.com>
2018-07-09 20:12:10 +0800

    部分修改

--- src/main/java/com/spring/worldwire/dao/ProductRequestDao.java
+++ src/main/java/com/spring/worldwire/dao/ProductRequestDao.java
@@ -2,6 +2,7 @@ package com.spring.worldwire.dao;
 
 import com.spring.worldwire.model.ProductRequest;
 import com.spring.worldwire.query.ProductRequestQuery;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -18,5 +19,5 @@ public interface ProductRequestDao {
 
     int updateByPrimaryKey(ProductRequest record);
 
-    List<ProductRequest> selectByQuery(ProductRequestQuery query);
+    List<ProductRequest> selectByQuery(@Param("query") ProductRequestQuery query);
 }
