c5f2bf9789f71855da3ab10542fc8946bf6a247f
luxun1 <luxun@jd.com>
2018-09-28 18:36:31 +0800

    修改需求列表

--- src/main/java/com/spring/worldwire/config/LocaleConfig.java
+++ src/main/java/com/spring/worldwire/config/LocaleConfig.java
@@ -1,32 +1,31 @@
 package com.spring.worldwire.config;
 
-import java.util.Locale;
-
 import com.spring.worldwire.companent.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class LocaleConfig {
 
-	@Bean(name = "localeResolver")
-	public LocaleResolver localeResolver() {
-		MyLocaleResolver slr = new MyLocaleResolver();
-		// 默认语言
-		slr.setDefaultLocale(Locale.US);
-		return slr;
-	}
+    @Bean(name = "localeResolver")
+    public LocaleResolver localeResolver() {
+        MyLocaleResolver slr = new MyLocaleResolver();
+        // 默认语言
+        slr.setDefaultLocale(Locale.CHINA);
+        return slr;
+    }
 
-	@Bean(name = "messageSource")
-	public ResourceBundleMessageSource resourceBundleMessageSource() {
-		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-		source.setBasename("messages/messages");
-		source.setFallbackToSystemLocale(false);
-		source.setDefaultEncoding("utf-8");
-		return source;
-	}
+    @Bean(name = "messageSource")
+    public ResourceBundleMessageSource resourceBundleMessageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages/messages");
+        source.setFallbackToSystemLocale(false);
+        source.setDefaultEncoding("utf-8");
+        return source;
+    }
 
 }
